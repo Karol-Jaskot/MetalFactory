26f7454223b3417d0c5de45182705bbad3cc446f
Karol-Jaskot <karol.jaskot@wp.pl>
2020-03-15 22:49:24 +0100

    Basic project architecture

---
+++ src/main/java/pl/project/metalfactory/MetalFactoryApplication.java
@@ -0,0 +1,13 @@
+package pl.project.metalfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/project/metalfactory/MetalFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.metalfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
